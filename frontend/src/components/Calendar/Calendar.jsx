0e92ec0aba5c8c2ae7340b3c653083dfebe48c47
Illia Kinzera <ilyakinzera@gmail.com>
2025-06-21 20:24:23 +0200

    Set up React project, set up the project structure and add new React components such as Calendar and Header. Header is an empty page, Calendar is working, by adding the Schedule-X calendar template mark-up.

---
+++ frontend/src/components/Calendar/Calendar.jsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import './CalendarStyle.css';
+import { ScheduleXCalendar, useCalendarApp } from '@schedule-x/react';
+import { createViewDay, createViewWeek, createViewMonthGrid, CalendarApp} from '@schedule-x/calendar';
+import"@schedule-x/theme-default/dist/calendar.css"
+import { createEventModalPlugin } from '@schedule-x/event-modal';
+import { createDragAndDropPlugin } from '@schedule-x/drag-and-drop';
+
+function Calendar() {
+
+  const calendar = useCalendarApp({
+    views: [
+      createViewDay(),
+      createViewWeek(),
+      createViewMonthGrid()
+    ],
+    events: [
+      {
+        id: 1,
+        title: "My first ever event",
+        start: "2025-06-20 14:00",
+        end: "2025-06-20 15:00",
+        description: "This is a test event"
+    }
+  ],
+    selectedDate: "2025-06-20",
+    plugins: [
+      createEventModalPlugin(),
+      createDragAndDropPlugin()
+    ]
+  });
+
+  return (
+    <>
+      <h1>Head</h1>
+      <div>
+        <ScheduleXCalendar calendarApp={calendar} />
+      </div>
+    </>
+    
+  );
+}
+
+export default Calendar;
